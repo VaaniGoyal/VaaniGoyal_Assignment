bfcc45ccab06f5c77f1026f3fd76b84739ec4f1a
VaaniGoyal <vanu.goyal@gmail.com>
2024-11-08 22:02:57 +0530

    Added server.js for backend

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors()); // Enable CORS for cross-origin requests
+app.use(bodyParser.json()); // Parse JSON request bodies
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 //App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes
-import { Header, Footer } from "./template";
 import Landing_Page from "./Landing_Page";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Footer />
       <div>
         <Routes>
           <Route path="/" element={<Landing_Page />} />
